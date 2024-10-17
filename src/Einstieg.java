4a0a4207550f67cda69a12a1a855ee81da910bab
EddyWayz <96652868+EddyWayz@users.noreply.github.com>
2024-10-17 16:23:49 +0200

    Add task from Aufgabenblatt_0

---
+++ src/Einstieg.java
@@ -0,0 +1,10 @@
+public class Einstieg {
+    public static void main(String[] args) {
+        System.out.println("Los geht's!");
+
+        int intVar = 42;
+        System.out.println(intVar);
+    }
+
+}
+
---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myAge = 18;
+        float mySize = 1.80F;
+        String myFirstName = "Edaurd";
+        String myLastName = "Wayz";
+        boolean iAmVegan = false;
+        if(iAmVegan) {
+            System.out.println("Mein Name ist " + myFirstName + " " + myLastName +", ich bin " + myAge + " Jahre alt und bin ein Veganer. Außerdem bin ich \" + mySize + \" m groß.\"");
+        } else {
+            System.out.println("Mein Name ist " + myFirstName + " " + myLastName +", ich bin " + myAge + " Jahre alt und bin kein Veganer. Außerdem bin ich " + mySize + " m groß.");
+        }
+    }
+}
