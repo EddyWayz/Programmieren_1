0e08e23dbf96130c87bf1c09709b19bce882230b
EddyWayz <96652868+EddyWayz@users.noreply.github.com>
2024-10-27 16:56:53 +0100

    Change "DateChecker.java"

--- src/DateChecker.java
+++ src/DateChecker.java
@@ -5,7 +5,7 @@ public class DateChecker {
       int day = In.readInt("Bitte geben Sie hier den Tag des zu prüfenden Tags ein: ");
       int month = In.readInt( "Bitte geben Sie hier den Monat des zu prüfenden Tags ein: ");
       int year = In.readInt( "Bitte geben Sie hier das Jahr des zu prüfenden Tags ein: ");
-      System.out.print(isLeapYear(year));
+      System.out.println(isValidDate(day, month, year));
   }
 
   static boolean isLeapYear(int year){
@@ -14,5 +14,55 @@ public class DateChecker {
     } else {
         return false;
     }
+  }
+
+  static int numberOfDays(int month, int year) {
+      switch (month) {
+          case 1:
+              return 31;
+          case 2:
+              if (isLeapYear(year)) {
+                  return 29;
+              } else {
+                  return 28;
+              }
+          case 3:
+              return 31;
+          case 4:
+              return 30;
+          case 5:
+              return 31;
+          case 6:
+              return 30;
+          case 7:
+              return 31;
+          case 8:
+              return 31;
+          case 9:
+              return 30;
+          case 10:
+              return 31;
+          case 11:
+              return 30;
+          case 12:
+              return 31;
+          default:
+              return -99;
+      }
+  }
+
+  static boolean isValidDate(int day, int month, int year) {
+      if (year < 0 ) {
+          System.out.println("Fehler 1");
+          return false;
+      }
+      if ((month < 1) || (month > 12)) {
+          System.out.println("Fehler 2");
+          return false;
+      }
+      if (day < 1 || day > numberOfDays(month, year)) {
+          return false;
+      }
+      return true;
   }
 }
