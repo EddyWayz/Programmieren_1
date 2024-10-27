4f1a8754556c5b8e5b72b9e6fcd143ff1397a7f8
EddyWayz <96652868+EddyWayz@users.noreply.github.com>
2024-10-27 16:21:04 +0100

    change "MehrwertsteuerCent.java" to the original

--- src/MehrwertsteuerCent.java
+++ src/MehrwertsteuerCent.java
@@ -14,4 +14,3 @@ public class MehrwertsteuerCent {
         System.out.println("Bei einem Nettobetrag von " + (double) (nettoPreis)/100 + " Euro, liegt die Mehrwertsteuer bei " + (double) (mehrwertsteuer)/100 + " Euro. \n"+"Deshalb liegt der Bruttobetrag bei: " + (double) (bruttobetrag)/100 + " Euro.");
     }
 }
-// Test
