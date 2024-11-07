c84d481ddce3d8e9ffc30d4ae1cfd4cdd8c3d2fe
EddyWayz <96652868+EddyWayz@users.noreply.github.com>
2024-11-07 12:54:35 +0000

    Finish “Sudoku.java”

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,12 +1,17 @@
+/**
+* Die Klasse Sudoku erstellt ein lösbares Sudoku.
+* 
+*/
+
 public class Sudoku {
   public static void main(String[] args) {
     int[][] grid = new int[9][9];
     
-    // Printet leeres Sudoku-Gitter
+    // Printet leeres Sudoku-Gitter.
     System.out.println("Leeres Sudoku: ");
     showSudoku(grid);
 
-    // Befüllt Sudoku mit Werten eines echten Sudokus und gibt es auf der Konsole aus
+    // Befüllt Sudoku mit Werten eines echten Sudokus und gibt es auf der Konsole aus.
     insert(grid,0,1,3);
     insert(grid,0,7,7);
     insert(grid,1,3,5);
@@ -26,19 +31,20 @@ public class Sudoku {
     insert(grid,7,6,3);
     insert(grid,8,7,4);
     System.out.println("\nEin echtes, lösbares Sudoku: ");
-    showSudoku(grid);
-    
+    showSudoku(grid); 
   }
   
   /**
   * Zeigt das übergebene Gitter auf der Konsole aus. 
   * Wagerechte Trennlinien sind über die Konstante dividingLines anpassbar.
-  *@param int[][] grid ist das auszugebende Gitter.
+  *@param grid ist das auszugebende Gitter. (int[][])
   */
    static void showSudoku(int[][] grid){
     final String dividingLines = "-------------------------";
      
-    System.out.println(dividingLines);
+    System.out.println(dividingLines); // oberen wagerechten Trennlinien.
+
+    // Erstellt das 3*3 Sudoku Feld mit senkrechten und wagerechten Trennlinien. 
     for(int i = 0; i < grid.length; i++){
       System.out.print("| ");
       for(int j = 0; j < grid[i].length; j++){
@@ -51,7 +57,6 @@ public class Sudoku {
           System.out.print("| ");
         }
       }
-
       if((i+1)%3 == 0){
         System.out.print("\n"+dividingLines);
       }
@@ -60,11 +65,14 @@ public class Sudoku {
   }
   
   /**
-  *
+  * Diese Funktion schreibt eine Zahl von 1 bis 9 in ein bestimmtes Feld des Sudokus.
+  * @param grid ist das zweidimensionale Gitter des Sudokus. (int[][])
+  * @param row ist die Reihe, in die die Zahl eingeschrieben werden soll. (int)
+  * @param col ist die Spalte, in die die Zahl eingeschrieben werden soll. (int)
+  * @param num ist die einzuschreibende Zahl von 1 bis 9. (int)
   */
   static void insert(int[][] grid, int row, int col, int num){
     if((row < 0) || (row > 8) || (col < 0) || (col > 8) || (num < 1) || (num > 9)){
-      System.out.println("Fehler: ");
       return; 
     } 
     grid[row][col] = num; 
